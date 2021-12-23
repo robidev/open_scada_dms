f141a00a954af8b9fc0b36d9588d275b1a8d746f
Robin Massink <robin.massink@alliander.com>
2021-12-24 00:46:59 +0100

    replaced panzoom with leaflet (for drawing support)

--- client/static/client.js
+++ client/static/client.js
@@ -1,4 +1,4 @@
-var socket, schema_panzoomcontrol, schema_svgRoot, schema_in_view, schema_svgElementData, gis_svgRoot, gis_in_view, gis_map, geojsonlayer;
+var socket, schema_leafletmap, schema_svgRoot, schema_in_view, svglayer, gis_svgRoot, gis_in_view, gis_map, geojsonlayer;
 
 function init_gis(){
   geojsonlayer = {};
@@ -77,56 +77,28 @@ function exportGeoJSON(featureGroup) {
 }
 
 function init_schema(){
-  schema_svgElementData = {};
+  svglayer = {};
   schema_svgRoot = {};
   schema_in_view = [];
-  //register data for svg
-  var schema_map = document.getElementById("mmi_svg");
-  schema_map.style.display = "block";
-  //it's important to add an load event listener to the object, as it will load the svg doc asynchronously
-  //but sometimes the svg loads before this event listener is created
-  if(schema_map.getSVGDocument() == null){//normal situation where svg is loaded later
-    schema_map.addEventListener("load",function(){ 
-      var svgDoc = schema_map.contentDocument; //get the inner DOM of mmi.svg
-      schema_svgRoot  = svgDoc.documentElement;
-      schema_panzoomcontrol = svgPanZoom(schema_map, { 
-        zoomEnabled: true, 
-        controlIconsEnabled: true, 
-        minZoom: 0.1, 
-        maxZoom: 10, 
-        onZoom: update_schema, 
-        onPan: update_schema ,
-        preventMouseEventsDefault: false,
-        fit: false,
-      });
-      schema_panzoomcontrol.pan({x:1,y:1});
-    },false);
-  }
-  else {//race condition, where svg is allready loaded from cache
-    var svgDoc = schema_map.contentDocument; //get the inner DOM of mmi.svg
-    schema_svgRoot  = svgDoc.documentElement;
-    schema_panzoomcontrol = svgPanZoom(schema_map, { 
-      zoomEnabled: true, 
-      controlIconsEnabled: true, 
-      minZoom: 0.1, 
-      maxZoom: 10, 
-      onZoom: update_schema, 
-      onPan: update_schema ,
-      preventMouseEventsDefault: false,
-      fit: false,
-    });
-    schema_panzoomcontrol.pan({x:1,y:1});
-  }
+  var schema_div = document.getElementById("mmi_svg");
+  schema_div.style.display = "block";
+
+  schema_leafletmap = L.map('mmi_svg', { renderer: L.svg(), crs: L.CRS.Simple }).setView([0,0], -10);
+  var bounds = [[0,0], [1000,1000]];
+  svglayer = L.geoJSON().addTo(schema_leafletmap,bounds);
+  schema_leafletmap.on('moveend', update_schema);
+  schema_leafletmap.on('zoomend', update_schema);
+  schema_leafletmap.setZoom(-9);
+  //schema_leafletmap.fitBounds(bounds);
 }
 
 function deinit_schema(){
   var schema_map = document.getElementById("mmi_svg");
   schema_map.style.display = "none";
-  schema_panzoomcontrol.destroy();
+  schema_leafletmap.remove();
   schema_svgRoot.innerHTML = "";
-
-  delete schema_panzoomcontrol;
-  delete schema_svgElementData;
+  delete svglayer;
+  delete schema_leafletmap;
   delete schema_svgRoot;
   delete schema_in_view;
 }
@@ -145,7 +117,7 @@ function deinit_gis(){
 
 function toggle_view() {
   var gis = document.getElementById("gis_map");
-  var schema = document.getElementById("mmi_svg");
+  var schema_div = document.getElementById("mmi_svg");
   if (gis.style.display === "none") {
     deinit_schema();
     init_gis();
@@ -172,90 +144,6 @@ $(document).ready(function() {
     var element = data['element'];
     var value = data['data']['value'];
     var type = data['data']['type'];
-
-    if(schema_svgRoot != null){//if the svg is loaded
-      //check for each occurence of id, there can be multiple instances of the same id, with different classes
-      $("#" + $.escapeSelector(element),schema_svgRoot).each(function(idx, el){
-        var cl = el.classList.toString();
-
-
-        if( cl == "MEAS"){
-          if(el.dataset.size > 0 || typeof(el.dataset.text) === "undefined"){
-            var desc = el.innerHTML;
-            el.textContent = value;
-          }
-          else{
-            var val = abbreviate_number(parseFloat(value),0)
-            el.textContent = el.dataset.text.replace("{value}",val);
-          }
-
-        }
-        if(cl == "XCBR" || cl == "XSWI"){
-          if(type == 'boolean'){
-            if(value=='True'){
-              if(schema_svgElementData[el.id]['position'] != true) {
-                $("#open",el)[0].beginElement();
-                schema_svgElementData[el.id]['position'] = true;
-
-                var ref = el.id;
-                var sibling = $(el).siblings(".CSWI");//find a CSWI sibling, and operate on that instead
-                if(sibling.length > 0){
-                  ref = sibling[0].id;
-                }
-                schema_svgElementData[ref]['position'] = true;
-              }
-            }
-            else{
-              if(schema_svgElementData[el.id]['position'] != false){
-                $("#close",el)[0].beginElement();
-                schema_svgElementData[el.id]['position'] = false;
-
-                var ref = el.id;
-                var sibling = $(el).siblings(".CSWI");//find a CSWI sibling, and operate on that instead
-                if(sibling.length > 0){
-                  ref = sibling[0].id;
-                }
-                schema_svgElementData[ref]['position'] = false;
-              }
-            }
-          }
-          if(type == 'bit-string'){
-            if(value == '1'){
-              if(schema_svgElementData[el.id]['position'] != false) {
-                $("#close",el)[0].beginElement();
-                schema_svgElementData[el.id]['position'] = false;
-
-                var ref = el.id;
-                var sibling = $(el).siblings(".CSWI");//find a CSWI sibling, and operate on that instead
-                if(sibling.length > 0){
-                  ref = sibling[0].id;
-                }
-                schema_svgElementData[ref]['position'] = false;
-              }
-            }
-            else if(value == '2'){
-              if(schema_svgElementData[el.id]['position'] != true){
-                $("#open",el)[0].beginElement();
-                schema_svgElementData[el.id]['position'] = true;
-
-                var ref = el.id;
-                var sibling = $(el).siblings(".CSWI");//find a CSWI sibling, and operate on that instead
-                if(sibling.length > 0){
-                  ref = sibling[0].id;
-                }
-                schema_svgElementData[ref]['position'] = true;
-              }
-            }
-            else if(value == '0'){
-              $("#transition",el)[0].beginElement();
-            }
-            else{
-              $("#error",el)[0].beginElement();
-            }
-          }
-        }
-      })
-    }
   });
 
   socket.on('svg_object_add_to_schema', function (data) {
@@ -266,67 +154,36 @@ $(document).ready(function() {
     }
 
     schema_in_view.push(data['id']);
-    //register values in this svg for reporting
+
       //register for all values in loaded svg
     $("g",node).find("*").each(function(idx, el){
       var cl = el.classList.toString();
-      console.log("node:" + node.id + ", id:" + el.id + ", class:" + cl);
-      //elements that can be interacted with by the IEC61850 client
+      //console.log("el.id:" + el.id + " cl:"+cl);
       if(el.id.startsWith("iec60870://") == true){    
-        schema_svgElementData[el.id] = {};
-        if(cl == "XCBR"){
-          //socket.emit('register_datapoint', {id : el.id, class : cl});
-          console.log("XCBR");
-          $("#close",el)[0].beginElement();
-        }
-        if(cl == "XSWI"){
-          //socket.emit('register_datapoint', {id : el.id, class : cl});
-          console.log("XCBR");
-          $("#open",el)[0].beginElement();
-        }
-        if(cl == "MEAS"){
-          //socket.emit('register_datapoint', {id : el.id, class : cl});
-        }
-      }
-      else
-      {
-        //if(cl == "EXT"){
-        //  el.onclick = extLink;
-        //}
+        if(cl == "XCBR"){ $("#close",el)[0].beginElementAt(1.0); }
+        if(cl == "XSWI"){ $("#open",el)[0].beginElementAt(1.0); }
       }
     });
   });
 
   socket.on('svg_object_remove_from_schema', function (data) {
     //remove svg from object
-    var obj = schema_svgRoot.firstChild.querySelector("#"+data);
+    var obj = schema_svgRoot[data];
     if(obj != null){
       //unregister values in this svg
-      $("g",node).find("*").each(function(idx, el){
-        console.log("node:" + node.id + ", id:" + el.id );
+      $("g",obj).find("*").each(function(idx, el){
         var cl = el.classList.toString();
-        //elements that can be interacted with by the IEC60870 client
         if(el.id.startsWith("iec60870://") == true){    
-          schema_svgElementData[el.id] = {};
-          if(cl == "XCBR"){
-            //socket.emit('unregister_datapoint', {id : el.id, class : cl});
-          }
-          if(cl == "XSWI"){
-            //socket.emit('unregister_datapoint', {id : el.id, class : cl});
-          }
-          if(cl == "MEAS"){
-            //socket.emit('unregister_datapoint', {id : el.id, class : cl});
-          }
+          //unregister
         }
       });
-      //svg_unload(svg)
       obj.remove();
-
       var index = schema_in_view.indexOf(data);
       if (index > -1) {
         schema_in_view.splice(index, 1);
       }
     }
+
   });
 
   socket.on('svg_object_add_to_gis', function (data) {
@@ -387,35 +244,27 @@ $(document).ready(function() {
 
 
 var update_schema = function(){ 
-  pos = this.getPan();
-  zoom = this.getZoom();
-  socket.emit('get_svg_for_schema', {'x': pos.x, 'y': pos.y, 'z': zoom, 'in_view': schema_in_view, 'width': this.getSizes().width, 'height': this.getSizes().height});
+  pos = schema_leafletmap.getCenter();
+  zoom = 100 + schema_leafletmap.getZoom();
+  bounds = schema_leafletmap.getBounds();
+
+  socket.emit('get_svg_for_schema', {'x': bounds.getWest(), 'y': bounds.getSouth(), 'z': zoom, 'in_view': schema_in_view, 'width': bounds.getEast()-bounds.getWest(), 'height': bounds.getNorth()- bounds.getSouth()});
   //socket.emit('get_svg', {data: ''} );
-  //console.log(zoom + " - " + pos.x + ", " + pos.y + ", in_view:" + schema_in_view + ", width:" + this.getSizes().width, ", height:" + this.getSizes().height); 
+  console.log('w:' + bounds.getWest() + ' n:' + bounds.getNorth() + ' e:' + bounds.getEast() + ' s:' + bounds.getSouth());
+  console.log(schema_leafletmap.getZoom())
 } 
 
 function svg_add_to_schema(x, y, x2, y2, svgString, svgId) {
-  var obj = schema_svgRoot.firstChild.querySelector("#"+svgId);
-  if(obj != null){
-    return null;
-  }
-  var parser = new DOMParser();
-  var doc = parser.parseFromString(svgString, "image/svg+xml");
-  var svgNode = doc.documentElement;
+  schema_svgRoot[svgId] = new DOMParser().parseFromString(svgString, "image/svg+xml").documentElement;
+  var dimension = "0 0 " + (x2-x).toString() + " " + (y2-y).toString();
+  schema_svgRoot[svgId].setAttribute('viewBox', dimension );
 
-  schema_svgRoot.firstChild.appendChild(svgNode);//firstchild is svg, as pan/zoom adds a parent
-  var newNode = schema_svgRoot.firstChild.lastChild;//firstchild is svg, as pan/zoom adds a parent
-  newNode.id = svgId;
-  newNode.setAttribute("x", x.toString());
-  newNode.setAttribute("y", y.toString());
+  L.svgOverlay(schema_svgRoot[svgId], [ [ y,x], [ y2,x2 ] ]).addTo(schema_leafletmap);
 
-  //var dimension = "0 0 " + (x2-x).toString() + " " + (y2-y).toString();
-  //newNode.setAttribute('preserveAspectRatio', 'none');
-  //newNode.setAttribute('viewBox', dimension );
-  //newNode.setAttribute('transform',"scale(0.5,1)");
-  //newNode.setAttribute("width", x2-x);
-  //newNode.setAttribute("height", (y2-y));
-  return newNode;
+  schema_svgRoot[svgId].id = svgId;
+  return schema_svgRoot[svgId];
+
+
 }
 
 var update_gis = function(){ 
